292351a808bcbb271fbab29389e5b63096a36ace
hyunsoo-Oh <hyunsoo2245@gmail.com>
2025-08-11 09:48:43 +0900

    Add MPU6050 Library

---
+++ IMU/MPU6050/ap.h
@@ -0,0 +1,16 @@
+/*
+ * ap.h
+ *
+ *  Created on: Aug 10, 2025
+ *      Author: jweli
+ */
+
+#ifndef AP_H_
+#define AP_H_
+
+#include "hw.h"
+
+void apInit(void);
+void apMain(void);
+
+#endif /* AP_H_ */
